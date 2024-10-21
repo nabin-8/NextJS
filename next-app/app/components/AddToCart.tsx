bf1faaf31e0c8ad3a0c98785daa0300d672d091c
nabin-8 <nabinacharya740@gmail.com>
2024-10-21 13:11:01 +0545

    next: Fundamentals CSR, SSR, Data Fetching, Caching, Dynamic Rendering

---
+++ next-app/app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+function AddToCart() {
+  return (
+    <>
+        <button onClick={()=> console.log('Click')}>Add to Cart</button>
+    </>
+  )
+}
+
+export default AddToCart
