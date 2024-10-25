7237ef0b750c5833eaf168cab41a542bad83d64e
nabin-8 <nabinacharya740@gmail.com>
2024-10-25 19:14:46 +0545

    next style: tailwind daisyUI

--- next-app/app/components/ProductCart.tsx
+++ next-app/app/components/ProductCart.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import AddToCart from './AddToCart';
+// import styles from './ProductCart.module.css'
+
 
 const ProductCart = () => {
   return (
--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 interface User{
   id:number;
   name:string;
+  email:string;
 }
 
 const UsersPage = async () => {
@@ -15,9 +16,20 @@ const UsersPage = async () => {
     <>
       <h1>Users</h1>
       <p>{new Date().toLocaleTimeString()}</p>
-      <ul>
-          {users.map(user => <li key={user.id}>{user.name}</li>)}
-      </ul>
+      <table className='table tab-border'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>)}
+        </tbody>
+      </table>
     </>
   )
 }
